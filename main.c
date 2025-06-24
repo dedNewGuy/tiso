d29fab87ca5311ef314dbaee994e15c3bb701b38
dedNewGuy <aminkafrinewgen@gmail.com>
2025-06-24 18:07:50 +0800

    separate thread for input handling

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <pthread.h>
 #include "term.h"
+
+void *input_handling(void *arg);
 
 int main()
 {
-    char buf[5];
-    size_t nbytes;
+    pthread_t input_thread;
+    
     float sleep_time = 1;
     int keepRunning = 1;
 
     assert(tty_raw(STDIN_FILENO) != -1);
 
+    pthread_create(&input_thread, NULL, input_handling, &keepRunning);
 
-    nbytes = sizeof(buf);
     while (keepRunning) {
 	printf(CURSOR_HIDE);
 	printf(SCREEN_PUSH);
@@ -21,19 +25,31 @@ int main()
 	printf(CURSOR_HOME);
 
 	printf("Hello, World!\n");
-	
-	read(STDIN_FILENO, buf, nbytes);
-	if ((int)buf[0] == 3 || buf[0] == 'q') {
-	    keepRunning = 0;
-	}
 
 	term_sleep(sleep_time);
     }
 
-    assert(tty_reset(STDIN_FILENO) != -1);
-    
-    printf(SCREEN_POP);
-    printf(CURSOR_SHOW);
     
     return 0;
+}
+
+void *input_handling(void *keepRunning)
+{
+    char buf[5];
+    size_t nbytes = sizeof(buf);
+
+    int run = *(int*)keepRunning;
+
+    while (run) {
+	read(STDIN_FILENO, buf, nbytes);
+	if ((int)buf[0] == 3 || buf[0] == 'q') {
+	    assert(tty_reset(STDIN_FILENO) != -1);
+    
+	    printf(SCREEN_POP);
+	    printf(CURSOR_SHOW);
+	    exit(0);
+	}
+    }
+
+    return NULL;
 }
