c3b842182a53480505d3b8accc65b478e0c5823f
dedNewGuy <aminkafrinewgen@gmail.com>
2025-06-25 22:05:01 +0800

    basic timer engine feature

--- tiso.c
+++ tiso.c
@@ -5,20 +5,26 @@
 #include "term.h"
 
 void *input_handling(void *arg);
+void restore_term_state(void);
 
-int main()
+void timer_descend(int *hour, int *minute, int *second);
+
+int main(void)
 {
     pthread_t input_thread;
     
     float sleep_time = 1;
     int keepRunning = 1;
 
-    time_t t;
-    struct tm b_time;
-
     assert(tty_noncanon() != -1);
 
     pthread_create(&input_thread, NULL, input_handling, &keepRunning);
+
+    int hour = 1;
+    int minute = 0;
+    int second = 0;
+
+    char frame_buffer[64];
 
     while (keepRunning) {
 	printf(CURSOR_HIDE);
@@ -27,13 +33,12 @@ int main()
 	printf(SCREEN_CLEAR);
 	printf(CURSOR_HOME);
 
-	t = time(NULL);
-	b_time = *localtime(&t);
-
-	printf("now: %d-%02d-%02d %02d:%02d:%02d\n", b_time.tm_year + 1900
-	       , b_time.tm_mon + 1, b_time.tm_mday, b_time.tm_hour, b_time.tm_min, b_time.tm_sec);
+	sprintf(frame_buffer, "%02d:%02d:%02d", hour, minute, second);
+	printf("%s\n", frame_buffer);
 
 	term_sleep(sleep_time);
+
+	timer_descend(&hour, &minute, &second);
     }
 
     
@@ -50,13 +55,38 @@ void *input_handling(void *keepRunning)
     while (run) {
 	read(STDIN_FILENO, buf, nbytes);
 	if ((int)buf[0] == 3 || buf[0] == 'q') {
-	    assert(reset_tty_mode() != -1);
-    
-	    printf(SCREEN_POP);
-	    printf(CURSOR_SHOW);
-	    exit(0);
+	    restore_term_state();
 	}
     }
 
     return NULL;
+}
+
+void restore_term_state(void)
+{
+    assert(reset_tty_mode() != -1);
+    
+    printf(SCREEN_POP);
+    printf(CURSOR_SHOW);
+    exit(0);
+}
+
+void timer_descend(int *hour, int *minute, int *second)
+{
+    if (*second > 0) {
+	*second -= 1;
+    } else {
+	if (*minute > 0) {
+	    *minute -= 1;
+	    *second = 59;
+	} else {
+	    if (*hour > 0) {
+		*hour -= 1;
+		*minute = 59;
+		*second = 59;
+	    } else {
+		restore_term_state();
+	    }
+	}
+    }
 }
